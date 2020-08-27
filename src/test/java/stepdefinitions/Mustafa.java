870276d8a1bf037b303f6687181622f2485df5c6
Muska <67065937+Muskar@users.noreply.github.com>
2020-08-27 10:42:51 +0200

    mustafa dan ikinci commit

--- src/test/java/stepdefinitions/Mustafa.java
+++ src/test/java/stepdefinitions/Mustafa.java
@@ -7,6 +7,7 @@ public class Mustafa {
     public void test(){
         System.out.println("Mustafa dan herkese selamlar");
         System.out.println("mustafa dan mustafa ya ilk deneme");
-
+        System.out.println("Mustafa dan herkese selamlar");
+        System.out.println("mustafa dan mustafa ya ilk deneme");
     }
 }
