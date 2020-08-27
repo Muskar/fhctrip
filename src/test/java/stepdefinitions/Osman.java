21a1ae71a15b035c52c4bb85e771cc6aa9d9d523
Osman <osmanaykan34@gmail.com>
2020-08-27 11:22:13 +0200

    Osman

--- src/test/java/stepdefinitions/Osman.java
+++ src/test/java/stepdefinitions/Osman.java
@@ -6,5 +6,9 @@ public class Osman {
     @Test
     public void test(){
         System.out.println("Osman dan herkese selamlar");
+        System.out.println("Ikinci commit Osman");
+        System.out.println("Ücüncü Osman");
+        System.out.println("Son");
+        System.out.println("Merhaba");
     }
 }
